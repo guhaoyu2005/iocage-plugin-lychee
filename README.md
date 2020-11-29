f4139870f140f1b4a35f69e6a7adeffcf6f42f6b
guhaoyu2005 <guhaoyu2005@gmail.com>
2020-11-29 00:35:47 -0500

    Added files

---
+++ README.md
@@ -0,0 +1,16 @@
+# TrueNAS/FreeNAS/FreeBSD ```iocage``` Lychee Plugin.
+
+An iocage installation script for [Lychee: A great looking and easy-to-use photo-management-system.](https://lycheeorg.github.io) [src](https://github.com/LycheeOrg/Lychee).
+
+
+## Installation Instructions.
+
+```
+curl -o /tmp/lychee.json https://raw.githubusercontent.com/guhaoyu2005/iocage-plugin-lychee/master/lychee.json
+iocage fetch -P /tmp/lychee.json -n lychee ip4_addr="igb0|10.0.0.1"
+```
+
+# Acknowledgments
+
+[This script follows Lychee's official installation guide.](https://lycheeorg.github.io/docs/installation.html)
+[Iocage script guide](https://www.ixsystems.com/documentation/freenas/11.3-U5/plugins.html#create-a-plugin)
---
+++ post_install.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+
+# Enable the service
+sysrc -f /etc/rc.conf nginx_enable="YES"
+sysrc -f /etc/rc.conf php_fpm_enable="YES"
+sysrc -f /etc/rc.conf mysql_enable="YES"
+
+# Create php.ini
+cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
+sed -i '' 's/.*max_execution_time =.*/max_execution_time = 200/' /usr/local/etc/php.ini
+sed -i '' 's/.*post_max_size =.*/post_max_size = 100M/' /usr/local/etc/php.ini
+sed -i '' 's/.*upload_max_filesize =.*/upload_max_filesize = 100M/' /usr/local/etc/php.ini
+sed -i '' 's/.*memory_limit =.*/memory_limit = 256M/' /usr/local/etc/php.ini
+sed -i '' 's/.*max_file_uploads =.*/max_file_uploads = 100/' /usr/local/etc/php.ini
+
+# Create www.conf
+cp /usr/local/etc/php-fpm.d/www.conf.default /usr/local/etc/php-fpm.d/www.conf
+sed -i '' 's/;listen.owner/listen.owner/' /usr/local/etc/php-fpm.d/www.conf
+sed -i '' 's/;listen.group/listen.group/' /usr/local/etc/php-fpm.d/www.conf
+sed -i '' 's/;listen.mode/listen.mode/' /usr/local/etc/php-fpm.d/www.conf
+sed -i '' 's/.*listen = 127.0.0.1:9000.*/listen = \/tmp\/php7.4-fpm.sock/' /usr/local/etc/php-fpm.d/www.conf
+
+# Generate sql credential
+USER="lychee"
+DB="lychee"
+PASS=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1`
+export LC_ALL=C
+
+# Write credential into plugin_config file
+echo "Database Name: $DB" > /root/PLUGIN_INFO
+echo "Database User: $USER" >> /root/PLUGIN_INFO
+echo "Database Password: $PASS" >> /root/PLUGIN_INFO
+echo "Nextcloud Admin Password: $PASS" >> /root/PLUGIN_INFO
+
+# Fetch lychee
+mkdir /usr/local/www/lychee
+cd /usr/local/www/lychee
+git clone --recurse-submodules https://github.com/LycheeOrg/Lychee.git .
+
+# Start services
+service php-fpm start
+sleep 5
+service mysql-server start
+sleep 5
+
+# Init sql
+mysql -u root --connect-expired-password <<-EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${PASS}';
+ALTER USER 'mysql'@'localhost' IDENTIFIED BY '${PASS}';
+CREATE DATABASE \`${DB}\` DEFAULT CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci';
+CREATE USER '${USER}'@'localhost' IDENTIFIED BY '${PASS}';
+GRANT ALL PRIVILEGES ON ${DB}.* TO '${USER}'@'localhost';
+FLUSH PRIVILEGES;
+EOF
+
+# Prepare lychee
+cd /usr/local/www/lychee/
+composer install --no-dev
+chown -R www:www /usr/local/www/lychee
+chmod -R 755 /usr/local/www/lychee
+
+cp /usr/local/www/lychee/.env.example /usr/local/www/lychee/.env
+sed -i '' 's/DB_CONNECTION=.*/DB_CONNECTION=mysql/' /usr/local/www/lychee/.env
+sed -i '' 's/DB_HOST=.*/DB_HOST=127.0.0.1/' /usr/local/www/lychee/.env
+sed -i '' 's/DB_PORT=.*/DB_PORT=3306/'  /usr/local/www/lychee/.env
+sed -i '' "s/DB_DATABASE=.*/DB_DATABASE=${DB}/"  /usr/local/www/lychee/.env
+sed -i '' "s/DB_USERNAME=.*/DB_USERNAME=${USER}/" /usr/local/www/lychee/.env
+sed -i '' "s/DB_PASSWORD=.*/DB_PASSWORD=${PASS}/" /usr/local/www/lychee/.env
+
+cd /usr/local/www/lychee/
+php artisan key:generate
+php artisan migrate
+
+# Start nginx
+service nginx start
